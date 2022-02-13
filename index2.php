a646036f81131e0a9e640d629fcc163c226e0d1f
irhamsky <irham_21520026@stimata.ac.id>
2022-02-13 12:26:59 +0700

    Penulisan Variabel

---
+++ index2.php
@@ -0,0 +1,17 @@
+<?php
+ $nama = "Irham";
+ ?>
+
+ <!DOCTYPE html>
+ 
+ <html>
+ <head>
+          <title>
+              Belajar PHP
+          </title>
+ </head>
+ <body>
+     <h1>Hallo, Selamat Datang <?php echo $nama; ?></h1>
+     
+ </body>
+ </html>
